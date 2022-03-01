d66a701bb66ba4f604994fb0dc5cdb24049a2086
Rafli <muhammadraflipembalap@gmail.com>
2022-03-01 11:25:13 +0700

    restaurant-app-finished-first-commmit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_main_ui/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          scaffoldBackgroundColor: Colors.grey[50],
+          primarySwatch: Colors.deepOrange,
+          primaryColor: Colors.deepOrangeAccent,
+        ),
+        home: HomeScreen());
+  }
+}
